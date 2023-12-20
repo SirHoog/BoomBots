39ee605c3bb604dd182bba9fe502c2000baa9fc3
SirHoog <121210119+SirHoog@users.noreply.github.com>
2023-12-19 20:47:16 -0700

    Trying to fix bugs

--- Simulation/AI/Brain.gd
+++ Simulation/AI/Brain.gd
@@ -20,9 +20,12 @@ func vectorDotProduct(vector1, vector2):
 	if vector1.size() != vector2.size():
 		print("ERROR: Vectors must have the same length for dot product calculation.")
 		
+		print(vector1.size())
+		print(vector2.size())
+		
 		return []
 	
-	for i in vector1.size():
+	for i in range(vector1.size()):
 		product += vector1[i] * vector2[i]
 	
 	return product
@@ -30,7 +33,7 @@ func vectorDotProduct(vector1, vector2):
 func vectorAdd(vector1, vector2):
 	var addend : Array[float] = []
 	
-	for i in vector1.size():
+	for i in range(vector1.size()):
 		addend.append(vector1[i] + vector2[i])
 	
 	return addend
@@ -40,7 +43,7 @@ var layers : Array[Array] = []
 func _ready():
 	var raycasts : Node = get_node("../Raycasts")
 	
-	for i in RaycastsAmount:
+	for i in range(RaycastsAmount):
 		var raycast : RayCast2D = RayCast2D.new()
 		
 		raycast.set_target_position(Vector2(
@@ -52,7 +55,7 @@ func _ready():
 func _init():
 	var inputLayer : Array[Neuron] = []
 	
-	for i in RaycastsAmount + 1:
+	for i in range(RaycastsAmount + 1):
 		if i == RaycastsAmount + 1: # If bias neuron
 			inputLayer.append(Neuron.new(1, [], HiddenLayerSize))
 		else:
@@ -60,25 +63,25 @@ func _init():
 	
 	layers.append(inputLayer)
 	
-	for i in HiddenLayersAmount:
+	for i in range(HiddenLayersAmount):
 		var hiddenLayer : Array[Neuron] = []
 		
-		for j in HiddenLayerSize + 1:
+		for j in range(HiddenLayerSize + 1):
 			if i == HiddenLayersAmount: # If last hidden layer
 				if j == HiddenLayerSize + 1: # If bias neuron
-					hiddenLayer.push_back(Neuron.new(1, [], OutputLayerSize))
+					hiddenLayer.append(Neuron.new(1, [], OutputLayerSize))
 				else:
-					hiddenLayer.push_back(Neuron.new(randi_range(-1, 1), [], OutputLayerSize))
+					hiddenLayer.append(Neuron.new(randi_range(-1, 1), [], OutputLayerSize))
 			else:
 				if j == HiddenLayerSize + 1: # If bias neuron
-					hiddenLayer.push_back(Neuron.new(1, [], HiddenLayerSize))
+					hiddenLayer.append(Neuron.new(1, [], HiddenLayerSize))
 				else:
-					hiddenLayer.push_back(Neuron.new(randi_range(-1, 1), [], HiddenLayerSize))
+					hiddenLayer.append(Neuron.new(randi_range(-1, 1), [], HiddenLayerSize))
 		
 		layers.append(hiddenLayer)
 	var outputLayer : Array[Neuron] = []
 	
-	for i in OutputLayerSize: # No `+ 1` because the output layer has no bias neuron
+	for i in range(OutputLayerSize): # No `+ 1` because the output layer has no bias neuron
 		outputLayer.append(Neuron.new(0, [], 0))
 	
 	layers.append(outputLayer)
@@ -88,24 +91,20 @@ func Update(input : Array[float]):
 	var weights : Array[float] = []
 	var biases : Array[float] = []
 	
-	for i in RaycastsAmount:
+	for i in range(RaycastsAmount):
 		layers.front()[i].activation = tanh(input[i]) # Normalize input
 	
 	biases.append(layers.front().back().activation) # Last neuron of input is the bias neuron
 	
-	for i in range(1, layers.size()):
+	for i in range(1, layers.size()): # For each layer (besides input layer)
 		var previousLayer : Array[Neuron] = layers[i - 1]
 		var currentLayer : Array[Neuron] = layers[i]
 		
-		for j in range(previousLayer.size() - 1):
-			var rowOfWeights : Array[float] = []
-			
+		for j in range(previousLayer.size() - 1): # For each neuron (besides bias neuron)
 			activations.append(previousLayer[j].activation)
 			
-			for k in range(currentLayer.size()):
-				rowOfWeights.append(previousLayer[j].weights[k])
-			
-			weights.append(rowOfWeights)
+			for k in range(currentLayer.size() - 1): # For each weight
+				weights.append(previousLayer[j].weights[k])
 		
 		### THE MATH PART ###
 		var layerActivations : Array[float] = vectorAdd(vectorDotProduct(weights, activations), biases)
--- Simulation/AI/Neuron.gd
+++ Simulation/AI/Neuron.gd
@@ -4,10 +4,11 @@ class_name Neuron
 var activation : float = randi_range(-1, 1)
 var weights : Array[float] = []
 
-func _init(_activation : float = randi_range(-1, 1), _weights : Array[float] = [], weightCount : int = 0):
+func _init(_activation : float = randi_range(-1, 1), _weights : Array[float] = [], weightsCount : int = 0):
 	activation = _activation
-	weights = _weights
 	
-	if weightCount != 0:
-		for i in weightCount:
+	if (weightsCount != 0):
+		for i in range(weightsCount):
 			weights.append(randi_range(-1, 1))
+	else:
+		weights = _weights
